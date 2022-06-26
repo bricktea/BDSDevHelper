42e7f9f11bdf5b2ea3e455876704856f3de838e5
RedbeanW <redbeana44945@gmail.com>
2022-06-26 20:48:47 +0800

    add gen_genealogy (incomplete)

--- bdsdevhelper.py
+++ bdsdevhelper.py
@@ -1,3 +1,4 @@
+from glob import glob
 import idaapi
 import ida_nalt
 import pyperclip as pc
@@ -7,6 +8,14 @@ from ida_kernwin import *
 import tkinter.filedialog as askFile
 import tkinter.messagebox as msgBox
 import json
+import xmind
+from xmind.core.const import *
+from xmind.core.markerref import MarkerId
+from xmind.core.topic import TopicElement
+import xml.dom
+import tempfile
+import os
+import zipfile
 
 _ver = '1.0.0'
 _call = '__fastcall'
@@ -62,6 +71,8 @@ class Hooks(idaapi.UI_Hooks):
                 print("[x] Register action '%s' failed!" % action.NAME)
         
         ida_kernwin.attach_action_to_menu("Edit/BDSDevHelper/Load original data", loadOriginalData.NAME, ida_kernwin.SETMENU_INS)
+        # ida_kernwin.attach_action_to_menu("Edit/BDSDevHelper/Generate type tree", genGenealogy.NAME, ida_kernwin.SETMENU_INS)
+        
         print('[*] BDS Dev Helper is loaded, ver %s.' % _ver)
         print('[*] By: RedbeanW.')
 
@@ -93,6 +104,82 @@ def changeVariableType(ea, lvar, tpe):
 #------------------------------------------------------------------------------
 # Registered Actions
 #------------------------------------------------------------------------------
+
+class genGenealogy(ida_kernwin.action_handler_t):
+
+    ###
+    ### Incomplete.
+    ### xmind need to sort all elements, but...
+    ###
+
+    NAME = 'helper:genGenealogy'
+    TEXT = "Generate type tree"
+    TOOLTIP = "Generate genealogy using originalData"
+
+    def __init__(self, core):
+        ida_kernwin.action_handler_t.__init__(self)
+        self.core = core
+
+    def activate(self, ctx):
+        
+        # load original data.
+        file_name = askFile.askopenfilename(
+            title = 'Chose originalData json.',
+            filetypes = [('JSON','*.json'),('All files','*')],
+            initialdir = os.getcwd()
+            )
+        print('[+] Original data chosed: %s' % file_name)
+        print('[+] Parsing ...')
+        file_data = open(file_name)
+        original_data = json.load(file_data)
+        file_data.close()
+
+        # ask .xmind path.
+        file_name = askFile.asksaveasfilename(
+            title = 'Where to save .xmind?',
+            filetypes = [('XMind File','*.xmind'),('All files','*')],
+            initialdir = os.getcwd()
+            )
+        if file_name[len(file_name)-6:] != '.xmind':
+            file_name = file_name + '.xmind'
+        
+        print('[+] XMind file on: %s' % file_name)
+
+        workbook = xmind.load(file_name)
+        sheet1 = workbook.getPrimarySheet()
+        sheet1.createElement('test')
+        root_topic = sheet1.getRootTopic()
+        root_topic.setTitle('Bedrock Dedicated Server')
+
+        applied = dict()
+        for typeName,data in original_data['classes'].items():
+            if not 'parent_types' in data:
+                topic = root_topic.addSubTopic(topics_type=TOPIC_DETACHED)
+                topic.setTitle(typeName)
+                applied[typeName] = topic
+        
+        classes_len = len(original_data['classes'])
+        loop_time = 0
+        while len(applied) != classes_len:
+            loop_time += 1
+            print('loop time %d' % loop_time)
+            if loop_time == 100:
+                print('loop times error, jumpout!')
+                break
+            for typeName,data in original_data['classes'].items():
+                if 'parent_types' in data:
+                    for i in data['parent_types']:
+                        if i in applied:
+                            topic = root_topic.addSubTopic(topics_type=TOPIC_DETACHED)
+                            topic.setTitle(typeName)
+                            sheet1.createRelationship(applied[i].getID(),topic.getID())
+                            applied[typeName] = topic            
+
+        xmind.save(workbook, path=file_name)
+        XMindFixer().fixXMindFile(file_name)
+
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_ALWAYS
 
 class loadOriginalData(ida_kernwin.action_handler_t):
     NAME = 'helper:loadOriginalData'
@@ -456,9 +543,88 @@ class asSymCall(ida_kernwin.action_handler_t):
 
 PLUGIN_ACTIONS = \
 [
+    genGenealogy,
     loadOriginalData,
     asTHook,
     asTInstanceHook,
     asTClasslessInstanceHook,
     asSymCall
 ]
+
+## XMindFixer类来源：
+## https://github.com/noemotionLi/xmind-2021-fix
+## 做了少许修改，感谢原作者
+
+class XMindFixer():
+
+    def get_filename(self, file_name):
+        f = zipfile.ZipFile(file_name)
+        path_list = []
+
+        for name in f.namelist():
+            temp_list = name.split('/')
+            temp_str = ''
+            for i in range(name.count('/')+1):
+                temp_str += '/' + temp_list[i]
+                temp = temp_str.replace('/','',1)
+                path_list.append(temp)
+        
+        path_list = list(set(path_list))
+        path_list.insert(0,'META-INF')
+        path_list.insert(1,'META-INF/manifest.xml')
+        f.close()
+
+        def type_check(file_path):
+            if os.path.splitext(file_path)[1] == '.xml':
+                type_media = 'text/xml'
+            elif os.path.splitext(file_path)[1] == '.png':
+                type_media = 'image/png'
+            else : 
+                type_media = ''
+                file_path += '/'
+            return type_media,file_path
+        
+        temp_list = []
+        for wrong_path in path_list:
+            media_type,file_path = type_check(wrong_path)
+            temp_list.append([media_type,file_path])
+        
+        return temp_list
+
+    def xml_writer(self, Attr_value):
+        domImp = xml.dom.getDOMImplementation()
+        doc = domImp.createDocument(None, None, None)
+        rootNode = doc.createElement("manifest")
+
+        doc.appendChild(rootNode)
+        doc.createAttribute("password-hint")
+        doc.createAttribute("xmlns")
+        doc.createAttribute("media-type")
+        doc.createAttribute("full-path")
+        rootNode.setAttribute("password-hint","")
+        rootNode.setAttribute("xmlns","urn:xmind:xmap:xmlns:manifest:1.0")
+
+        def add_node(root, value):
+            child = doc.createElement("file-entry")
+            root.appendChild(child)
+            child.setAttribute("media-type",value[0])
+            child.setAttribute("full-path",value[1])
+        
+        for value in Attr_value:
+            add_node(rootNode, value)
+        
+        return doc
+
+
+    def fixXMindFile(self, path):
+        target_list = self.get_filename(path)
+        xml_doc = self.xml_writer(target_list)
+        directory_name = tempfile.mkdtemp()
+        xml_path = os.path.join(directory_name,"manifest.xml")
+
+        with open(os.path.join(xml_path), 'w', encoding='utf-8') as writer:
+            xml_doc.writexml(writer, indent='\n', addindent="", newl="", encoding="utf-8")
+        
+        f = zipfile.ZipFile(path,'a')
+        f.write(xml_path,'META-INF/manifest.xml')
+        f.close()
