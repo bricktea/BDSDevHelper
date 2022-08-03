0e35499782da78642e1518189e76e68d1d67f883
RedbeanW <redbeana44945@gmail.com>
2022-08-03 10:52:34 +0800

    1.1.0

--- BDSDevHelper.py
+++ BDSDevHelper.py
@@ -8,14 +8,9 @@ from ida_kernwin import *
 import tkinter.filedialog as askFile
 import tkinter.messagebox as msgBox
 import json
-import xmind
-from xmind.core.const import *
-import xml.dom
-import tempfile
 import os
-import zipfile
 
-_ver = '1.0.0'
+_ver = '1.1.0'
 _call = '__fastcall'
 
 #------------------------------------------------------------------------------
@@ -70,7 +65,6 @@ class Hooks(idaapi.UI_Hooks):
         
         # ida_kernwin.attach_action_to_menu("Edit/BDSDevHelper/Load original data", loadOriginalData.NAME, ida_kernwin.SETMENU_INS)
         ida_kernwin.attach_action_to_menu("Edit/BDSDevHelper/Export structure", exportStructure.NAME, ida_kernwin.SETMENU_INS)
-        # ida_kernwin.attach_action_to_menu("Edit/BDSDevHelper/Generate type tree", genGenealogy.NAME, ida_kernwin.SETMENU_INS)
         
         print('[*] BDS Dev Helper is loaded, ver %s.' % _ver)
         print('[*] By: RedbeanW.')
@@ -78,35 +72,45 @@ class Hooks(idaapi.UI_Hooks):
 hooks = Hooks()
 hooks.hook()
 
-def fixIdaBugAndGetArguments(func):
-    m = func.argidx
-    n = func.arguments
-    if m[0] == 1:
-        n[0],n[1] = n[1],n[0]
-    return n
+#------------------------------------------------------------------------------
+# Utils
+#------------------------------------------------------------------------------
 
-def cleanType(name,NoPointer = False):
-    rtn = str()
-    for t in name.split(' '):
-        if t != 'const' and t != 'struct' and t != 'class':
-            rtn = rtn + t + ' '
-    if NoPointer:
-        rtn = rtn.replace('*','')
-    return rtn.strip()
+class Utils():
 
-def changeVariableType(ea, lvar, tpe):
-    lsi = lvar_saved_info_t()
-    lsi.ll = lvar
-    lsi.type = tpe
-    return modify_user_lvar_info(ea, MLI_TYPE, lsi)
+    @staticmethod
+    def fixIdaBugAndGetArguments(func):
+        m = func.argidx
+        n = func.arguments
+        if m[0] == 1:
+            n[0],n[1] = n[1],n[0]
+        return n
 
-def getMemberType(sid, offset):
-    struct = ida_struct.get_struc(sid)
-    member = ida_struct.get_member(struct, offset)
-    tif = ida_typeinf.tinfo_t()
-    if ida_struct.get_member_tinfo(tif, member):
-        return tif.__str__() 
-    return ""    
+    @staticmethod
+    def cleanType(name,NoPointer = False):
+        rtn = str()
+        for t in name.split(' '):
+            if t != 'const' and t != 'struct' and t != 'class':
+                rtn = rtn + t + ' '
+        if NoPointer:
+            rtn = rtn.replace('*','')
+        return rtn.strip()
+
+    @staticmethod
+    def changeVariableType(ea, lvar, tpe):
+        lsi = lvar_saved_info_t()
+        lsi.ll = lvar
+        lsi.type = tpe
+        return modify_user_lvar_info(ea, MLI_TYPE, lsi)
+
+    @staticmethod
+    def getMemberType(sid, offset):
+        struct = ida_struct.get_struc(sid)
+        member = ida_struct.get_member(struct, offset)
+        tif = ida_typeinf.tinfo_t()
+        if ida_struct.get_member_tinfo(tif, member):
+            return tif.__str__() 
+        return ""    
 
 #------------------------------------------------------------------------------
 # Registered Actions
@@ -158,7 +162,7 @@ class exportStructure(ida_kernwin.action_handler_t):
             for offset,mem,size in idautils.StructMembers(sid):
                 members.append({
                     'name': mem,
-                    'type': getMemberType(sid,offset),
+                    'type': Utils.getMemberType(sid,offset),
                     'offset': offset,
                     'size': size
                 })
@@ -170,82 +174,6 @@ class exportStructure(ida_kernwin.action_handler_t):
         with open(file_name,'w') as file:
             file.write(json.dumps(j,indent=4))
         print('[+] structure data saved to: %s' % file_name)
-
-    def update(self, ctx):
-        return ida_kernwin.AST_ENABLE_ALWAYS
-
-class genGenealogy(ida_kernwin.action_handler_t):
-
-    ###
-    ### Incomplete.
-    ### xmind need to sort all elements, but...
-    ###
-
-    NAME = 'helper:genGenealogy'
-    TEXT = "Generate type tree"
-    TOOLTIP = "Generate genealogy using originalData"
-
-    def __init__(self, core):
-        ida_kernwin.action_handler_t.__init__(self)
-        self.core = core
-
-    def activate(self, ctx):
-        
-        # load original data.
-        file_name = askFile.askopenfilename(
-            title = 'Chose originalData json.',
-            filetypes = [('JSON','*.json'),('All files','*')],
-            initialdir = os.getcwd()
-            )
-        print('[+] Original data chosed: %s' % file_name)
-        print('[+] Parsing ...')
-        file_data = open(file_name)
-        original_data = json.load(file_data)
-        file_data.close()
-
-        # ask .xmind path.
-        file_name = askFile.asksaveasfilename(
-            title = 'Where to save .xmind?',
-            filetypes = [('XMind File','*.xmind'),('All files','*')],
-            initialdir = os.getcwd()
-            )
-        if file_name[len(file_name)-6:] != '.xmind':
-            file_name = file_name + '.xmind'
-        
-        print('[+] XMind file on: %s' % file_name)
-
-        workbook = xmind.load(file_name)
-        sheet1 = workbook.getPrimarySheet()
-        sheet1.createElement('test')
-        root_topic = sheet1.getRootTopic()
-        root_topic.setTitle('Bedrock Dedicated Server')
-
-        applied = dict()
-        for typeName,data in original_data['classes'].items():
-            if not 'parent_types' in data:
-                topic = root_topic.addSubTopic(topics_type=TOPIC_DETACHED)
-                topic.setTitle(typeName)
-                applied[typeName] = topic
-        
-        classes_len = len(original_data['classes'])
-        loop_time = 0
-        while len(applied) != classes_len:
-            loop_time += 1
-            print('loop time %d' % loop_time)
-            if loop_time == 100:
-                print('loop times error, jumpout!')
-                break
-            for typeName,data in original_data['classes'].items():
-                if 'parent_types' in data:
-                    for i in data['parent_types']:
-                        if i in applied:
-                            topic = root_topic.addSubTopic(topics_type=TOPIC_DETACHED)
-                            topic.setTitle(typeName)
-                            sheet1.createRelationship(applied[i].getID(),topic.getID())
-                            applied[typeName] = topic            
-
-        xmind.save(workbook, path=file_name)
-        XMindFixer().fixXMindFile(file_name)
 
     def update(self, ctx):
         return ida_kernwin.AST_ENABLE_ALWAYS
@@ -323,13 +251,13 @@ class loadOriginalData(ida_kernwin.action_handler_t):
             handle_para(original_data['classes']['DirectoryPackWithEncryptionAccessStrategy']['public'][0]['params'][1]) + ' a3',
         ]
         print(a)
-        b = fixIdaBugAndGetArguments(cfunc)
+        b = Utils.fixIdaBugAndGetArguments(cfunc)
         b[0].set_lvar_type(a[0])
         b[1].set_lvar_type(a[1])
         b[2].set_lvar_type(a[2])
-        changeVariableType(ea,b[0],a[0])
-        changeVariableType(ea,b[1],a[1])
-        changeVariableType(ea,b[2],a[2])
+        Utils.changeVariableType(ea,b[0],a[0])
+        Utils.changeVariableType(ea,b[1],a[1])
+        Utils.changeVariableType(ea,b[2],a[2])
         
 
     """
@@ -382,16 +310,16 @@ class asTHook(ida_kernwin.action_handler_t):
             return
         func_data = decompile(ea)
         func_detail = str(func_data.type)
-        return_type = cleanType(func_detail[0:func_detail.find(_call)])
+        return_type = Utils.cleanType(func_detail[0:func_detail.find(_call)])
         call_args = str()
         return_args = str()
         FirstArg = True
-        for i in fixIdaBugAndGetArguments(func_data):
+        for i in Utils.fixIdaBugAndGetArguments(func_data):
             if FirstArg and i.name == 'this':
                 arg_name = 'self'
             else:
                 arg_name = i.name
-            call_args = call_args + cleanType(str(i.tif)) + ' ' + arg_name + ', '
+            call_args = call_args + Utils.cleanType(str(i.tif)) + ' ' + arg_name + ', '
             return_args = return_args + arg_name + ', '
         call_args = call_args[0:len(call_args)-2]
         return_args = return_args[0:len(return_args)-2]
@@ -447,16 +375,16 @@ class asTInstanceHook(ida_kernwin.action_handler_t):
             return
         func_data = decompile(ea)
         func_detail = str(func_data.type)
-        return_type = cleanType(func_detail[0:func_detail.find(_call)])
+        return_type = Utils.cleanType(func_detail[0:func_detail.find(_call)])
         call_args = str()
         return_args = str()
         FirstArg = True
-        for i in fixIdaBugAndGetArguments(func_data):
+        for i in Utils.fixIdaBugAndGetArguments(func_data):
             if FirstArg:
-                this_pointer_type = cleanType(str(i.tif),True)
+                this_pointer_type = Utils.cleanType(str(i.tif),True)
                 FirstArg = False
             else:
-                call_args = call_args + cleanType(str(i.tif)) + ' ' + i.name + ', '
+                call_args = call_args + Utils.cleanType(str(i.tif)) + ' ' + i.name + ', '
                 return_args = return_args + i.name + ', '
         call_args = call_args[0:len(call_args)-2]
         return_args = return_args[0:len(return_args)-2]
@@ -513,15 +441,15 @@ class asTClasslessInstanceHook(ida_kernwin.action_handler_t):
             return
         func_data = decompile(ea)
         func_detail = str(func_data.type)
-        return_type = cleanType(func_detail[0:func_detail.find(_call)])
+        return_type = Utils.cleanType(func_detail[0:func_detail.find(_call)])
         call_args = str()
         return_args = str()
         FirstArg = True
-        for i in fixIdaBugAndGetArguments(func_data):
+        for i in Utils.fixIdaBugAndGetArguments(func_data):
             if FirstArg:
                 FirstArg = False
                 continue
-            call_args = call_args + cleanType(str(i.tif)) + ' ' + i.name + ', '
+            call_args = call_args + Utils.cleanType(str(i.tif)) + ' ' + i.name + ', '
             return_args = return_args + i.name + ', '
         call_args = call_args[0:len(call_args)-2]
         return_args = return_args[0:len(return_args)-2]
@@ -580,10 +508,10 @@ class asSymCall(ida_kernwin.action_handler_t):
             return
         func_data = decompile(ea)
         func_detail = str(func_data.type)
-        return_type = cleanType(func_detail[0:func_detail.find(_call)])
+        return_type = Utils.cleanType(func_detail[0:func_detail.find(_call)])
         call_types = str()
-        for i in fixIdaBugAndGetArguments(func_data):
-            call_types = call_types + cleanType(str(i.tif)) + ', '
+        for i in Utils.fixIdaBugAndGetArguments(func_data):
+            call_types = call_types + Utils.cleanType(str(i.tif)) + ', '
         call_types = call_types[0:len(call_types)-2]
 
         # handles.
@@ -613,88 +541,9 @@ class asSymCall(ida_kernwin.action_handler_t):
 PLUGIN_ACTIONS = \
 [
     exportStructure,
-    genGenealogy,
     loadOriginalData,
     asTHook,
     asTInstanceHook,
     asTClasslessInstanceHook,
     asSymCall
 ]
-
-## XMindFixer类来源：
-## https://github.com/noemotionLi/xmind-2021-fix
-## 做了少许修改，感谢原作者
-
-class XMindFixer():
-
-    def get_filename(self, file_name):
-        f = zipfile.ZipFile(file_name)
-        path_list = []
-
-        for name in f.namelist():
-            temp_list = name.split('/')
-            temp_str = ''
-            for i in range(name.count('/')+1):
-                temp_str += '/' + temp_list[i]
-                temp = temp_str.replace('/','',1)
-                path_list.append(temp)
-        
-        path_list = list(set(path_list))
-        path_list.insert(0,'META-INF')
-        path_list.insert(1,'META-INF/manifest.xml')
-        f.close()
-
-        def type_check(file_path):
-            if os.path.splitext(file_path)[1] == '.xml':
-                type_media = 'text/xml'
-            elif os.path.splitext(file_path)[1] == '.png':
-                type_media = 'image/png'
-            else : 
-                type_media = ''
-                file_path += '/'
-            return type_media,file_path
-        
-        temp_list = []
-        for wrong_path in path_list:
-            media_type,file_path = type_check(wrong_path)
-            temp_list.append([media_type,file_path])
-        
-        return temp_list
-
-    def xml_writer(self, Attr_value):
-        domImp = xml.dom.getDOMImplementation()
-        doc = domImp.createDocument(None, None, None)
-        rootNode = doc.createElement("manifest")
-
-        doc.appendChild(rootNode)
-        doc.createAttribute("password-hint")
-        doc.createAttribute("xmlns")
-        doc.createAttribute("media-type")
-        doc.createAttribute("full-path")
-        rootNode.setAttribute("password-hint","")
-        rootNode.setAttribute("xmlns","urn:xmind:xmap:xmlns:manifest:1.0")
-
-        def add_node(root, value):
-            child = doc.createElement("file-entry")
-            root.appendChild(child)
-            child.setAttribute("media-type",value[0])
-            child.setAttribute("full-path",value[1])
-        
-        for value in Attr_value:
-            add_node(rootNode, value)
-        
-        return doc
-
-
-    def fixXMindFile(self, path):
-        target_list = self.get_filename(path)
-        xml_doc = self.xml_writer(target_list)
-        directory_name = tempfile.mkdtemp()
-        xml_path = os.path.join(directory_name,"manifest.xml")
-
-        with open(os.path.join(xml_path), 'w', encoding='utf-8') as writer:
-            xml_doc.writexml(writer, indent='\n', addindent="", newl="", encoding="utf-8")
-        
-        f = zipfile.ZipFile(path,'a')
-        f.write(xml_path,'META-INF/manifest.xml')
-        f.close()
